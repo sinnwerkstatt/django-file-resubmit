18b7ce485841bf8e24df94a0752af100de16b33e
Matt Johnson <mdj2@pdx.edu>
2014-01-03 16:07:01 -0800

    Remove unnecessary import that messed up my monkey patch in another app

--- file_resubmit/__init__.py
+++ file_resubmit/__init__.py
@@ -4,8 +4,6 @@ from django.conf import settings
 from django.contrib.admin.options import FORMFIELD_FOR_DBFIELD_DEFAULTS
 from django.core.exceptions import ImproperlyConfigured
 
-from file_resubmit.admin import AdminResubmitImageWidget, AdminResubmitFileWidget
-
 
 if not 'file_resubmit' in settings.CACHES:
     raise ImproperlyConfigured("CACHES['file_resubmit'] is not defined in settings.py")
